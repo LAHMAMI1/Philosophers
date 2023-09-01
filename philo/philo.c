b8f02dee62d3fbee0848e2654903810196f218c4
Oussama Lahmami <o.lahmami.2000@gmail.com>
2023-09-01 02:54:24 +0100

    nothing

--- philo/philo.c
+++ philo/philo.c
@@ -6,40 +6,50 @@
 /*   By: olahmami <olahmami@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 10:18:33 by olahmami          #+#    #+#             */
-/*   Updated: 2023/08/30 03:51:52 by olahmami         ###   ########.fr       */
+/*   Updated: 2023/08/31 17:07:56 by olahmami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void init_philo(int ac, char **av, t_philo *philo)
+void init_philo(int ac, char **av, t_data *data)
 {
-	if (ac < 5 || ac > 6 || check_args(av))
-		return (printf("Error: wrong number of arguments\n"));
-	philo = (t_philo *)malloc(sizeof(t_philo));
-	philo->forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * philo->num_philo);
-	if (!philo || !philo->forks)
-		return (1);
-	philo->num_philo = check_atoi(av[1]);
-	philo->time_to_die = check_atoi(av[2]);
-	philo->time_to_eat = check_atoi(av[3]);
-	philo->time_to_sleep = check_atoi(av[4]);
-	philo->num_eat = -1;
+	data = (t_data *)malloc(sizeof(t_data));
+	data->forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * data->num_philo);
+	if (!data || !data->forks)
+		return (NULL);
+	data->num_philo = check_atoi(av[1]);
+	data->time_to_die = check_atoi(av[2]);
+	data->time_to_eat = check_atoi(av[3]);
+	data->time_to_sleep = check_atoi(av[4]);
+	data->num_t_eat = -1;
 	if (ac == 6)
-		philo->num_eat = check_atoi(av[5]);
-	philo->start_time = current_time_in_milliseconds();
+		data->num_t_eat = check_atoi(av[5]);
+	data->start_time = current_time_in_milliseconds();
 }
 
 int	main(int ac, char **av)
 {
 	t_philo *philo;
+	t_data *data;
+	int i;
+	int j;
 
-	init_philo(ac, av, philo);
-	while (philo->num_philo > 0)
+	if (ac < 5 || ac > 6 || check_args(av))
+		return (printf("Error: wrong number of arguments\n"));
+	init_philo(ac, av, data);
+	philo = (t_philo *)malloc(sizeof(t_philo) * data->num_philo);
+	if (!philo)
+		return (1);
+	i = 0;
+	while (i < data->num_philo)
 	{
-		pthread_create(philo->threads, NULL, &to_do, philo);
-		pthread_mutex_init(&philo->forks[philo->num_philo], NULL);
-		philo->num_philo--;
+		j = 1;
+		philo[i].id = j;
+		philo[i].data = data;
+		pthread_create(&philo[i].threads, NULL, to_do, &philo[i]);
+		j++;
+		i++;
 	}
 	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: olahmami <olahmami@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 10:19:15 by olahmami          #+#    #+#             */
-/*   Updated: 2023/08/30 03:25:03 by olahmami         ###   ########.fr       */
+/*   Updated: 2023/08/31 17:05:56 by olahmami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,31 @@
 # include <sys/time.h>
 # include <unistd.h>
 
+typedef struct s_philo	t_philo;
+
+typedef struct s_data
+{
+	int					num_philo;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					num_t_eat;
+	pthread_mutex_t		*forks;
+	long				start_time;
+	t_philo				*philos;
+}						t_data;
 typedef struct s_philo
 {
-	int				num_philo;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int				num_eat;
-	pthread_t		threads;
-	pthread_mutex_t	*forks;
-	long			start_time;
-}					t_philo;
+	pthread_t			threads;
+	int					id;
+	t_data				*data;
+}						t_philo;
 
-int					ft_isdigit(int c);
-int					check_atoi(char *str);
-int					check_args(char **av);
+int						ft_isdigit(int c);
+int						check_atoi(char *str);
+int						check_args(char **av);
 
-void				*to_do(void *arg);
-long current_time_in_milliseconds();
+void					*to_do(void *arg);
+long					current_time_in_milliseconds(void);
 
 #endif
