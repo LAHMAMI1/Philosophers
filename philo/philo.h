ee40aacfa5589bd482a57f0700e536ce9527ee54
Oussama Lahmami <o.lahmami.2000@gmail.com>
2023-09-03 04:30:01 +0100

    lock printf

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: olahmami <olahmami@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 10:19:15 by olahmami          #+#    #+#             */
-/*   Updated: 2023/09/02 00:01:02 by olahmami         ###   ########.fr       */
+/*   Updated: 2023/09/03 02:46:15 by olahmami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ typedef struct s_data
 	int					time_to_sleep;
 	int					num_time_eat;
 	pthread_mutex_t		*forks;
+	pthread_mutex_t		print;
 	long				start_time;
 	t_philo				*philos;
 }						t_data;
--- philo/to_do.c
+++ philo/to_do.c
@@ -6,7 +6,7 @@
 /*   By: olahmami <olahmami@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 21:21:14 by olahmami          #+#    #+#             */
-/*   Updated: 2023/09/02 04:18:21 by olahmami         ###   ########.fr       */
+/*   Updated: 2023/09/03 04:29:25 by olahmami         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,31 @@ void *to_do(void *arg)
 	while (1)
 	{
 		pthread_mutex_lock(&philo->data->forks[philo->id - 1]);
-		printf("%ld ms %d has taken a fork\n", current_time_in_milliseconds() - philo->data->start_time,philo->id);
+		pthread_mutex_lock(&philo->data->print);
+		printf("%ld ms %d has taken a left fork\n", current_time_in_milliseconds() - philo->data->start_time,philo->id);
+		pthread_mutex_unlock(&philo->data->print);
 		pthread_mutex_lock(&philo->data->forks[philo->id % philo->data->num_philo]);
-		printf("%ld ms %d has taken a fork\n", current_time_in_milliseconds() - philo->data->start_time,philo->id);
+		pthread_mutex_lock(&philo->data->print);
+		printf("%ld ms %d has taken a right fork\n", current_time_in_milliseconds() - philo->data->start_time,philo->id);
+		pthread_mutex_unlock(&philo->data->print);
+		pthread_mutex_lock(&philo->data->print);
 		printf("%ld ms %d is eating\n", current_time_in_milliseconds() - philo->data->start_time,philo->id);
+		pthread_mutex_unlock(&philo->data->print);
 		while (current_time_in_milliseconds() - philo->data->start_time < philo->data->time_to_eat)
-			usleep(100);
+			usleep(1000);
 		pthread_mutex_unlock(&philo->data->forks[philo->id - 1]);
 		pthread_mutex_unlock(&philo->data->forks[philo->id % philo->data->num_philo]);
+		pthread_mutex_lock(&philo->data->print);
 		printf("%ld ms %d is sleeping\n", current_time_in_milliseconds() - philo->data->start_time,philo->id);
+		pthread_mutex_unlock(&philo->data->print);
 		while (current_time_in_milliseconds() - philo->data->start_time < philo->data->time_to_sleep)
-			usleep(100);
+			usleep(1000);
+		pthread_mutex_lock(&philo->data->print);
 		printf("%ld ms %d is thinking\n", current_time_in_milliseconds() - philo->data->start_time,philo->id);
+		pthread_mutex_unlock(&philo->data->print);
+		pthread_mutex_lock(&philo->data->print);
 		printf("%ld ms %d died\n", current_time_in_milliseconds() - philo->data->start_time,philo->id);
+		pthread_mutex_unlock(&philo->data->print);
 	}
 	
 	return (NULL);
